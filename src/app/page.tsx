766803475c1832320433d1ab198224cef8e1bc6e
lucascanni <77814636+lucascanni@users.noreply.github.com>
2024-03-27 14:15:51 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default async function Home() {
   return (
     <main className="my-24 text-center">
-      <h1 className="text-2xl"> Bienvenue sur le site de convertisseur de cryptomonnaiespour  l'evaluation de l'estiam</h1>
+      <h1 className="text-2xl"> Bienvenue sur le site de convertisseur de cryptomonnaies pour  l'evaluation de l'estiam</h1>
       <div className="flex gap-2 justify-center mt-12">
         <Link
           className=" inline-block py-3 px-6 rounded-md font-bold text-black bg-[#627EEA]"
